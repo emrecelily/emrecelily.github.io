36c59f010c633ee36f11ef7c20fdbca6d7dc113f
emrecelily <emrecelilyldrm@gmail.com>
2023-12-22 11:25:57 +0300

    aa

--- gorev12.php
+++ gorev12.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Random Table Generator</title>
+    <title>Rastegele Sayı Tablosu</title>
 </head>
 <body>
 <?php
